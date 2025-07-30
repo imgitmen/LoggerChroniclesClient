e24d324868385b26674ada2b4db5722db18e988a
michelangelo simeone <michelangelo.simeone@gmail.com>
2025-07-30 18:10:06 +0200

    async method

--- src/LoggerChroniclesClient/client.py
+++ src/LoggerChroniclesClient/client.py
@@ -1,6 +1,6 @@
 from datetime import date
-import http.client as cl
 import requests
+import aiohttp
 
 class RequestResult:
     status_code: int
@@ -41,6 +41,28 @@ class HttpClient:
         result.status_code = response.status_code
         if not response.ok:
             result.errors = response.json()
+        else:
+            result.errors = None
         
         return result
-        
+    
+    async def BackupAsync(self, loggerTypeCode: str, loggerSerial: str, timestamp: date, file: str) -> RequestResult:
+        url = self.__create_backup_url()
+        response = None
+        result = None
+        async with aiohttp.ClientSession(headers={"X-API-Key":self.__apikey}) as session:
+            with open(file, 'rb') as f:
+                body = { "loggerTypeCode": loggerTypeCode, 
+                        "loggerSerial": loggerSerial, 
+                        "timestamp": timestamp, 
+                        "file": f
+                        }
+                async with session.post(url, data=body,) as response:
+                    result = RequestResult()
+                    result.status_code = response.status
+                    if not response.ok:
+                        result.errors = await response.json()
+                    else:
+                        result.errors = None
+                    
+        return result
